e067f05bc57069f494e087a43d2f6c867a77c877
jayiuk <jayiuk987@gmail.com>
2024-08-21 09:53:10 +0900

    data preprocessing

---
+++ data_merge.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+import os
+
+class data_concat():
+    def __init__(self, path, column_name):
+        self.path = path
+        self.column_name = column_name
+
+    def concat(self):
+        data_list = os.listdir(self.path)
+        df_list = pd.DataFrame(columns = self.column_name)
+        for data in data_list:
+            df = pd.read_csv(os.path.join(self.path, data))
+            df = df.query('순위 <= 15')
+            df_list = pd.concat([df_list, df])
+        return df_list

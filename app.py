0e9b1aa36cf58024d87c7cbf5eb764bc937d86a1
jayiuk <jayiuk987@gmail.com>
2024-09-02 17:50:24 +0900

    app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import pickle as pkl
+
+def new_data(G, ERA, W, NP, IP, TBF, QS, WAR, RA_9, career_year, WHIP, BB,current_salary):
+    pd.DataFrame([{'ERA' : ERA, 'W' : W, 'QS' : QS, 'WAR' : WAR, 'WHIP' : WHIP, 'NP/IP' : NP/IP, }])
